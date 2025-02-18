6cb50ff1523d5fe1a24f8ab1c2232bc8d8aaa81e
unknown <tanbhirhossaincs@gmail.com>
2025-02-18 14:34:24 +0600

    Setup VUE Inertia

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';  // Add this import
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vue({  // Ensure `vue()` is used correctly
+           
+        }),
+    ],
+});
